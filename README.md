7e22c44b7a13e1b9e160269df9424c06bc81f614
jaycee-cpu93 <jamescoding93@gmail.com>
2023-09-15 13:35:05 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# simple-to-do
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/stackblitz-starters-zjycuh)
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './style.css';
+
+export default function App() {
+  const [todo, setTodo] = useState('');
+  const [todos, setTodos] = useState([
+    { name: 'Buy Milk', isCompleted: false },
+    { name: 'Buy Akara', isCompleted: false },
+    { name: 'Buy Moi Mio', isCompleted: false },
+  ]);
+  const addToDo = (event) => {
+    // event.preventDefault();
+    // console.log('i want to do something');
+    // console.log(todos);
+    // setTodos([...todos, { name: todo, isCompleted: false }]);
+  };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('i want to do something');
+    console.log(todos);
+    setTodos([...todos, { name: todo, isCompleted: false }]);
+    setTodo('');
+  };
+
+  const handleChange = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const style = {
+    color: "blue",
+    borderColor: 'yellow',
+    backgroundColor: "green"
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input  type="text" onChange={handleChange} value={todo} />
+        <button style={style} type="submit">Add to do</button>
+      </form>
+
+      <div>
+        {todos.map((todo) => (
+          <p>{todo.name}</p>
+        ))}
+      </div>
+    </div>
+  );
+}
